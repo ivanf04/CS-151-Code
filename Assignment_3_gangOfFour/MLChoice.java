6b5625ae0d5e2211e8ee5feebcb316f382673147
ivanf04 <ifaranda2004@gmail.com>
2026-03-22 14:30:47 -0700

    Rough ML algorithm, needs testing

--- Assignment_3_gangOfFour/MLChoice.java
+++ Assignment_3_gangOfFour/MLChoice.java
@@ -11,12 +11,19 @@ public class MLChoice implements ChoiceAlgrorithm {
     private Deque<Move> history;
     private Map<List<Move>, Integer> recordedChoices;
 
+    /**
+     * default constructor sets n = 5
+     */
     public MLChoice() {
         this.n = 5;
         this.history = new ArrayDeque<>();
         this.recordedChoices = new HashMap<>();
     }
 
+    /**
+     * 
+     * @param numRecChoices sets the size of recorded sequences 
+     */
     public MLChoice(int numRecChoices) {
         this.n = numRecChoices;
         this.history = new ArrayDeque<>();
@@ -26,6 +33,8 @@ public class MLChoice implements ChoiceAlgrorithm {
     /**
      * Enqueues every input a game and deques when queue is of length n
      * check the hashmap after a move is added
+     * 
+     * @param move is the latest move in a game of RPS
      */
     public void addMove(Move move) {
         if(history.size() == n) {
@@ -52,7 +61,38 @@ public class MLChoice implements ChoiceAlgrorithm {
 
     @Override
     public Move determineMove() {
-        //TODO: implement machine learing algorithm 
-        return Move.fromInt(0);
+
+        int maxFequency = 0; 
+        List<Move> predictedMoves = new ArrayList<>();
+        /**
+         * TODO: implement machine learing algorithm 
+         * Check the current squence against the existing records, based off the last 
+         * opposing player choice choose the winning move
+         */
+        
+        //take the first four inputs in the queue, 
+        //match them with the corresponind sequences in the map
+        //take the one with the highest frequency and take the last Move from that sequence
+        //as the move to be played 
+        List<Move> lastFour = new ArrayList<>(history);
+        Iterator<Move> it  = history.descendingIterator();
+
+        for(int i = 0; i < n - 1 && it.hasNext(); i++){
+            lastFour.add(it.next());
+        }
+        Collections.reverse(lastFour);
+
+        for(List<Move> key : recordedChoices.keySet()) {
+            if(key.subList(0, n - 1).equals(lastFour)) {
+                //match found, update max frequency and predictedMoves
+                if(recordedChoices.get(key) >= maxFequency) {
+                    maxFequency = recordedChoices.get(key);
+                    predictedMoves = key; 
+                }
+            }
+        }
+
+        Move predicMove = predictedMoves.get(n - 1); 
+        return predicMove;
     }
 }
