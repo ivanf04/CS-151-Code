d8d6dca13eb44467fe91b864a3dfbf52a7c07525
Tanisha Prasad <107425494+tprasad02@users.noreply.github.com>
2026-03-22 17:20:18 -0700

    added saveData, recordRound

--- Assignment_3_gangOfFour/randomChoice.java
+++ Assignment_3_gangOfFour/randomChoice.java
@@ -1,21 +1,41 @@
 package Assignment_3_gangOfFour;
-import java.util.Random;
-/**
- * Random choice algorithm for computer player
- */
-public class randomChoice implements ChoiceAlgrorithm {
 
-    private Random random = new Random();
-    public randomChoice(){}
+import java.util.Random;
+
+/**
+ * Random choice algorithm for the computer player.
+ */
+public class randomChoice implements ChoiceAlgorithm {
+
+    private final Random random = new Random();
+
+    public randomChoice() {
+    }
 
     /**
-     * Outputs a move (Rock, paper or scissors) at random 
-     * 
-     * @return the randomly chosen move 
+     * Outputs a move (rock, paper, or scissors) at random.
+     *
+     * @return the randomly chosen move
      */
     @Override
     public Move determineMove() {
         int choice = random.nextInt(3) + 1;
         return Move.fromInt(choice);
+    }
+
+    /**
+     * Random strategy does not learn from previous rounds.
+     */
+    @Override
+    public void recordRound(Move humanMove, Move computerMove) {
+        // Does nothing
+    }
+
+    /**
+     * Random strategy has nothing to save.
+     */
+    @Override
+    public void saveData() {
+        // Does nothing
     }
 }
