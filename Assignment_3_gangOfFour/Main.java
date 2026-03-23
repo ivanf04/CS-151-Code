5949c74f8cf88026b0d6a2022462eb8f15b9d9ff
ivanf04 <ifaranda2004@gmail.com>
2026-03-23 16:06:19 -0700

    added cli argument

--- Assignment_3_gangOfFour/Main.java
+++ Assignment_3_gangOfFour/Main.java
@@ -1,22 +1,44 @@
 /**
-* Simple test program to run a playthrough of rock paper scissors(RPS)
-*/
+ * Simple test program to run a playthrough of rock paper scissors(RPS)
+ * 
+ * To compile and run the project,
+ * javac Assignment_3_gangOfFour/*.java
+ * 
+ * then to run Main.java
+ * java Assignment_3_gangOfFour.Main -m
+ */
 package Assignment_3_gangOfFour;
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * Test file for the ML algorithm 
+ * Run "java test4 -r" for random computer move selection
+ * Run "java test4 -m" for ML computer move selection
+ */
 public class Main {
-  /**
-  * Create player, game rule and scoreboard objects. 
-  * Pass them as arguments to Game() constructor and run game.play
-  */
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Player bob = new HumanPlayer("Bob", scanner);
-    Player computer = new ComputerPlayer("Computer1");
-    ClassicRPS classicRPS = new ClassicRPS();
-    Scoreboard scoreboard = new Scoreboard();
-    MoveHistory moveHistory = new MoveHistory();
-    Game game = new Game(bob, computer, classicRPS, scoreboard, moveHistory);
-    game.play();
-  }
+    public static void main(String[] args) {
+        MoveHistory moveHistory = new MoveHistory();
+        Scanner scanner = new Scanner(System.in);
+
+        Player human = new HumanPlayer("Human", scanner);
+
+        //Default = random 
+        boolean useRandom = true; 
+
+        //read CLI argument
+        if(args.length > 0){
+            if(args[0].equals("-m"))
+                useRandom = false;
+            } else if (args[0].equals("-r")) {
+            useRandom = true;
+        }
+
+        Player mLComputer = new ComputerPlayer("ML", useRandom, moveHistory);
+
+        ClassicRPS rule = new ClassicRPS();
+        Scoreboard scoreboard = new Scoreboard();
+
+        Game game = new Game(human, mLComputer, rule, scoreboard, moveHistory);
+        game.play();
+    }
 }
