6c17e71b52da1e1220aeb9e32ec9deab9ef781d1
Tanisha Prasad <107425494+tprasad02@users.noreply.github.com>
2026-03-22 17:19:32 -0700

    added recordRound, saveData

--- Assignment_3_gangOfFour/ChoiceAlgorithm.java
+++ Assignment_3_gangOfFour/ChoiceAlgorithm.java
@@ -1,11 +1,29 @@
 package Assignment_3_gangOfFour;
- /**
-  * Defines a choice alrogtihm for the computer player
-  */
-public interface ChoiceAlgrorithm {
+
+/**
+ * Defines a choice algorithm for the computer player.
+ */
+public interface ChoiceAlgorithm {
+
     /**
-     * 
-     * @return the move of the player determined by the corresponding algorithm
+     * Determines the computer player's next move.
+     *
+     * @return the move chosen by the algorithm
      */
     Move determineMove();
+
+    /**
+     * Records the completed round so learning-based algorithms
+     * can update their internal data.
+     *
+     * @param humanMove the human player's move
+     * @param computerMove the computer player's move
+     */
+    void recordRound(Move humanMove, Move computerMove);
+
+    /**
+     * Saves any learned data at the end of the game.
+     * Random algorithms can leave this empty.
+     */
+    void saveData();
 }
